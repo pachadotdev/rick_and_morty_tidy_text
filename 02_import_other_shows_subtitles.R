9156eca40285ddd69cb18e839abd762ce33a143d
Mauricio Vargas <mvargas@dcc.uchile.cl>
2017-11-05 14:22:04 -0300

    structured folders and contents, now it's easier to use by others

---
+++ 02_import_other_shows_subtitles.R
@@ -0,0 +1,222 @@
+# 1: load packages
+
+if (!require("pacman")) install.packages("pacman")
+#run sudo apt-get install libpoppler-cpp-dev to install pdftools
+p_load(data.table,dplyr,stringr,rvest,janitor)
+p_load_gh("fkeck/subtools")
+
+##########################################################################################
+
+# 2: scrap BoJack Horseman subtitles
+
+url_subtitles = read_html("https://www.opensubtitles.org/en/ssearch/sublanguageid-eng/idmovie-181349")
+
+subtitles_tables = url_subtitles %>%
+  html_nodes(xpath='//*[(@id = "search_results")]') %>%
+  html_table(fill = TRUE)
+
+links_2 = url_subtitles %>% 
+  html_nodes("a") %>% 
+  html_attr("href")
+
+titles_2 = url_subtitles %>% 
+  html_nodes("a") %>% 
+  html_attr("title")
+
+links_all_seasons = tibble(link = links_2, title = titles_2) %>% 
+  filter(!is.na(title)) %>% 
+  distinct(link, .keep_all = T) %>% 
+  mutate(link = str_replace_all(link, "^/en/search/", "https://www.opensubtitles.org/download/s/")) %>% 
+  filter(grepl("imdb", link)) %>%
+  mutate(title = str_replace_all(title, "\"BoJack Horseman\" ", ""),
+         season = c(rep("S01",12),rep("S02",13),rep("S03",12),rep("S04",11)),
+         episode = c(1:12,0:12,1:12,1:8,10:12),
+         episode = if_else(nchar(episode) == 1, paste0("E0",episode), paste0("E",episode)),
+         file_name = paste(season, episode, str_replace_all(title, "[[:punct:]]", ""), sep = "_"),
+         file_name = str_replace_all(file_name, " ", "_")) %>% 
+  select(episode, season, title, file_name, link)
+
+try(dir.create("02_other_shows_subtitles"))
+try(dir.create("02_other_shows_subtitles/bojack_horseman"))
+try(dir.create("02_other_shows_subtitles/bojack_horseman/subtitles"))
+try(dir.create("02_other_shows_subtitles/bojack_horseman/subtitles_extraction"))
+
+fwrite(links_all_seasons, "02_other_shows_subtitles/bojack_horseman_links_all_seasons.csv")
+
+for(j in 1:nrow(links_all_seasons)) {
+  if(!file.exists(paste0("02_other_shows_subtitles/bojack_horseman/subtitles/",links_all_seasons$file_name[j],".zip"))) {
+    Sys.sleep(sample(seq(0.5, 1.5, by=0.5), 1))
+    download.file(links_all_seasons$link[j], paste0("02_other_shows_subtitles/bojack_horseman/subtitles/",links_all_seasons$file_name[j],".zip"), method = "wget")
+  }
+}
+
+subtiles_zip = list.files("02_other_shows_subtitles/bojack_horseman/subtitles/", recursive = T) %>% 
+  paste0("02_other_shows_subtitles/bojack_horseman/subtitles/", .)
+
+for(j in 1:length(subtiles_zip)) {
+  try(dir.create(paste0("02_other_shows_subtitles/bojack_horseman/subtitles_extraction/",links_all_seasons$file_name[j])))
+  unzip(subtiles_zip[[j]], exdir = paste0("02_other_shows_subtitles/bojack_horseman/subtitles_extraction/",links_all_seasons$file_name[j]), overwrite = F)
+}
+
+# I deleted BoJack Horseman - 02x07 - Hank After Dark.en.srt bc of broken encoding
+
+try(file.remove("02_other_shows_subtitles/bojack_horseman/subtitles_extraction/S02_E07_Hank_After_Dark/BoJack Horseman - 02x07 - Hank After Dark.en.srt"))
+
+subtitles_srt = tibble(sub_location = list.files("02_other_shows_subtitles/bojack_horseman/subtitles_extraction/", recursive = T)) %>% 
+  mutate(sub_location = paste0("02_other_shows_subtitles/bojack_horseman/subtitles_extraction/",sub_location),
+         folder = as.character(str_extract_all(sub_location, ".*/")),
+         file_size = file.size(sub_location)) %>% 
+  group_by(folder) %>% 
+  filter(file_size == max(file_size)) %>% 
+  distinct(folder, .keep_all = T) %>% 
+  mutate(episode_name = str_replace_all(sub_location, "02_other_shows_subtitles/bojack_horseman/subtitles_extraction/", ""),
+         episode_name = str_replace_all(episode_name, "/.*", ""),
+         season = str_replace_all(substr(episode_name,1,7), "_.*", ""),
+         episode = str_replace_all(substr(episode_name,1,7), ".*_", ""),
+         episode_name = str_replace_all(episode_name, "S[0-9][0-9]_E[0-9][0-9]_", ""),
+         episode_name = str_replace_all(episode_name, "_", " "))
+
+for(j in 1:nrow(subtitles_srt)) {
+  assign(paste(subtitles_srt$season[j],subtitles_srt$episode[j], sep = "_"),
+         as_tibble(read.subtitles(subtitles_srt$sub_location[j])$subtitles) %>%
+           clean_names() %>%
+           rename(linenumber = id) %>% 
+           filter(nchar(text) > 0) %>%
+           mutate(text = iconv(tolower(text)) ,
+                  season = subtitles_srt$season[j],
+                  episode = subtitles_srt$episode[j],
+                  episode_name = subtitles_srt$episode_name[j])
+  )
+}
+
+bojack_horseman_subs = mget(ls(pattern = "S[0-9][0-9]_E[0-9][0-9]")) %>% 
+  bind_rows()
+
+rm(list = ls(pattern = "S[0-9][0-9]_E[0-9][0-9]"))
+
+fwrite(bojack_horseman_subs, "02_other_shows_subtitles/bojack_horseman_subs.csv")
+
+list_subtitles_all = tibble(sub_location = list.files("02_other_shows_subtitles/bojack_horseman/subtitles_extraction/", recursive = T)) %>% 
+  mutate(sub_location = paste0("02_other_shows_subtitles/bojack_horseman/subtitles_extraction/",sub_location),
+         folder = as.character(str_extract_all(sub_location, ".*/")),
+         file_size = file.size(sub_location)) 
+
+list_subtitles_keep = list_subtitles_all %>% 
+  group_by(folder) %>% 
+  filter(file_size == max(file_size)) %>% 
+  distinct(folder, .keep_all = T)
+
+list_subtitles_remove = list_subtitles_all %>%   
+  anti_join(list_subtitles_keep)
+
+for(j in 1:nrow(list_subtitles_remove)) {
+  try(file.remove(list_subtitles_remove$sub_location[j]))
+}
+
+##########################################################################################
+
+# 3: scrap Gravity Falls subtitles
+
+url_subtitles = read_html("https://www.opensubtitles.org/en/ssearch/sublanguageid-eng/idmovie-131837")
+
+subtitles_tables = url_subtitles %>%
+  html_nodes(xpath='//*[(@id = "search_results")]') %>%
+  html_table(fill = TRUE)
+
+links_2 = url_subtitles %>% 
+  html_nodes("a") %>% 
+  html_attr("href")
+
+titles_2 = url_subtitles %>% 
+  html_nodes("a") %>% 
+  html_attr("title")
+
+links_all_seasons = tibble(link = links_2, title = titles_2) %>% 
+  filter(!is.na(title)) %>% 
+  distinct(link, .keep_all = T) %>% 
+  mutate(link = str_replace_all(link, "^/en/search/", "https://www.opensubtitles.org/download/s/")) %>% 
+  filter(grepl("imdb", link)) %>%
+  mutate(title = str_replace_all(title, "\"Gravity Falls\" ", ""),
+         season = c(rep("S01",20),rep("S02",20)),
+         episode = c(1:20,1:20),
+         episode = if_else(nchar(episode) == 1, paste0("E0",episode), paste0("E",episode)),
+         file_name = paste(season, episode, str_replace_all(title, "[[:punct:]]", ""), sep = "_"),
+         file_name = str_replace_all(file_name, " ", "_")) %>% 
+  select(episode, season, title, file_name, link)
+
+try(dir.create("02_other_shows_subtitles"))
+try(dir.create("02_other_shows_subtitles/gravity_falls"))
+try(dir.create("02_other_shows_subtitles/gravity_falls/subtitles"))
+try(dir.create("02_other_shows_subtitles/gravity_falls/subtitles_extraction"))
+
+fwrite(links_all_seasons, "02_other_shows_subtitles/gravity_falls_links_all_seasons.csv")
+
+for(j in 1:nrow(links_all_seasons)) {
+  if(!file.exists(paste0("02_other_shows_subtitles/gravity_falls/subtitles/",links_all_seasons$file_name[j],".zip"))) {
+    Sys.sleep(sample(seq(0.5, 1.5, by=0.5), 1))
+    download.file(links_all_seasons$link[j], paste0("02_other_shows_subtitles/gravity_falls/subtitles/",links_all_seasons$file_name[j],".zip"), method = "wget")
+  }
+}
+
+subtiles_zip = list.files("02_other_shows_subtitles/gravity_falls/subtitles/", recursive = T) %>% 
+  paste0("02_other_shows_subtitles/gravity_falls/subtitles/", .)
+
+for(j in 1:length(subtiles_zip)) {
+  try(dir.create(paste0("02_other_shows_subtitles/gravity_falls/subtitles_extraction/",links_all_seasons$file_name[j])))
+  unzip(subtiles_zip[[j]], exdir = paste0("02_other_shows_subtitles/gravity_falls/subtitles_extraction/",links_all_seasons$file_name[j]), overwrite = F)
+}
+
+# I deleted Gravity.Falls.S01E17.Boyz.Crazy.720p-Strajow.srt bc of broken encoding
+
+try(file.remove("02_other_shows_subtitles/gravity_falls/subtitles_extraction/S01_E17_Boyz_Crazy/Gravity.Falls.S01E17.Boyz.Crazy.720p-Strajow.srt"))
+
+subtitles_srt = tibble(sub_location = list.files("02_other_shows_subtitles/gravity_falls/subtitles_extraction/", recursive = T)) %>% 
+  mutate(sub_location = paste0("02_other_shows_subtitles/gravity_falls/subtitles_extraction/",sub_location),
+         folder = as.character(str_extract_all(sub_location, ".*/")),
+         file_size = file.size(sub_location)) %>% 
+  group_by(folder) %>% 
+  filter(file_size == max(file_size)) %>% 
+  distinct(folder, .keep_all = T) %>% 
+  mutate(episode_name = str_replace_all(sub_location, "02_other_shows_subtitles/gravity_falls/subtitles_extraction/", ""),
+         episode_name = str_replace_all(episode_name, "/.*", ""),
+         season = str_replace_all(substr(episode_name,1,7), "_.*", ""),
+         episode = str_replace_all(substr(episode_name,1,7), ".*_", ""),
+         episode_name = str_replace_all(episode_name, "S[0-9][0-9]_E[0-9][0-9]_", ""),
+         episode_name = str_replace_all(episode_name, "_", " "))
+
+for(j in 1:nrow(subtitles_srt)) {
+  assign(paste(subtitles_srt$season[j],subtitles_srt$episode[j], sep = "_"),
+         as_tibble(read.subtitles(subtitles_srt$sub_location[j])$subtitles) %>%
+           clean_names() %>%
+           rename(linenumber = id) %>% 
+           filter(nchar(text) > 0) %>%
+           mutate(text = iconv(tolower(text)) ,
+                  season = subtitles_srt$season[j],
+                  episode = subtitles_srt$episode[j],
+                  episode_name = subtitles_srt$episode_name[j])
+  )
+}
+
+gravity_falls_subs = mget(ls(pattern = "S[0-9][0-9]_E[0-9][0-9]")) %>% 
+  bind_rows()
+
+rm(list = ls(pattern = "S[0-9][0-9]_E[0-9][0-9]"))
+
+fwrite(gravity_falls_subs, "02_other_shows_subtitles/gravity_falls_subs.csv")
+
+list_subtitles_all = tibble(sub_location = list.files("02_other_shows_subtitles/gravity_falls/subtitles_extraction/", recursive = T)) %>% 
+  mutate(sub_location = paste0("02_other_shows_subtitles/gravity_falls/subtitles_extraction/",sub_location),
+         folder = as.character(str_extract_all(sub_location, ".*/")),
+         file_size = file.size(sub_location)) 
+
+list_subtitles_keep = list_subtitles_all %>% 
+  group_by(folder) %>% 
+  filter(file_size == max(file_size)) %>% 
+  distinct(folder, .keep_all = T)
+
+list_subtitles_remove = list_subtitles_all %>%   
+  anti_join(list_subtitles_keep)
+
+for(j in 1:nrow(list_subtitles_remove)) {
+  try(file.remove(list_subtitles_remove$sub_location[j]))
+}
